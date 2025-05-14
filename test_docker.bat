59f1a619b3b54a1496322246b1aa1aa44e6c50ff
Pedro Laurenti <pedrolaurenti04@gmail.com>
2025-05-14 11:21:24 -0300

    Merge branch 'main' of https://github.com/Pedro-Laurenti/maps-scrapping

--- test_docker.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-ECHO Construindo a imagem Docker...
-docker build -t gmaps-scraper .
-
-ECHO Executando o container com os parâmetros de teste...
-docker run -i gmaps-scraper < params.json
-
-ECHO Teste concluído.
-PAUSE
